c6ff42d04caf138edcc7f0bfdce163c5476689c0
evicno <229501549+evicno@users.noreply.github.com>
2025-11-17 11:14:02 +0100

    07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arrReverse = str.split("");
+    let strReverse = ""
+    for (let i = 0; i < str.length; i++) {
+        strReverse += arrReverse.pop();
+    }
+    return strReverse;
 };
 
 // Do not edit below this line
