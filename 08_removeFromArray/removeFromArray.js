9041646fc5361ebe1d76105efedd0560d490b462
evicno <229501549+evicno@users.noreply.github.com>
2025-11-17 15:29:18 +0100

    Complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...toRemove) {
+    let result = [];
+    for (item of arr) {
+        if (!toRemove.find(arg => arg === item)) {
+            result.push(item);
+        }
+    }           
+    return result;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
