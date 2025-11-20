2bb0faa18c092b19b0c0c7c5728dd60313d5fbba
evicno <229501549+evicno@users.noreply.github.com>
2025-11-20 20:14:32 +0100

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+let pureString = string.toLowerCase()
+                    .replaceAll(" ", "")
+                    .replaceAll(".", "")
+                    .replaceAll(",", "")
+                    .replaceAll("?", "")
+                    .replaceAll("!", "")
+                    .split("");
+let length = pureString.length;
+for (let i = 0; i < (length / 2 - 1); i++) {
+    if (pureString[i] != pureString[length - i - 1]) {
+        return false;
+    }
+}
+return true;
 };
 
 // Do not edit below this line
