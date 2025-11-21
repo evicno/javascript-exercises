54891b3c798f74b04503eb5b0e646991563abb6e
evicno <229501549+evicno@users.noreply.github.com>
2025-11-21 14:58:00 +0100

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    if (typeof(num) == "string") {
+        num = Number(num);
+    }
+    if (!typeof(num) == "number" || !Number.isInteger(num) || num < 0) {
+        return "OOPS";
+    }
+    let fibo = ["1", "1"];
+    if (num == 0) {
+        return 0;
+    }
+    else {
+        for (let i = 2; i <= num; i++) {
+            let sum = Number(fibo[i - 1]) + Number(fibo[i - 2]);
+            fibo.push(sum);
+        }
+    return Number(fibo[num - 1]);
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
