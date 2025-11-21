fb2e82f8706ac0d7f27015cc087c24102f7e35c7
evicno <229501549+evicno@users.noreply.github.com>
2025-11-21 15:43:27 +0100

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const d = new Date();
+    let year = d.getFullYear();
+    for (p of people) {
+        if (p.yearOfDeath === undefined){
+            p.yearOfDeath = year;
+        }
+    }
+    people.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+    return people[0];
 };
 
 // Do not edit below this line
