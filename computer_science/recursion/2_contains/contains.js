3546a02635585197e92440e18ded2c9c5653a65e
evicno <229501549+evicno@users.noreply.github.com>
2026-02-25 16:35:30 +0100

    Complete 2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,26 @@
-const contains = function() {
-  
+const contains = function (object, value) {
+  if (Number.isNaN(object) && Number.isNaN(value)) {
+    return object.value === value.value;
+  }
+  if (object === value) {
+    return true;
+  }
+  if (object !== Object(object)) {
+    // case object is primitive
+    return object === value;
+  } else {
+    // object is object or array
+    if (Array.isArray(object)) {
+      let found = false;
+      object.map((obj) => {
+        found = contains(obj, value) || found;
+      });
+      return found;
+    } else {
+      // it's an object
+      return contains(Object.values(object), value);
+    }
+  }
 };
-  
 // Do not edit below this line
 module.exports = contains;
